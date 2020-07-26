63e20235cf135ac104bd871e0a0c479d06817451
rmadulla <maddula.rajarahul@gmail.com>
2020-07-26 14:34:23 +0530

    Adding posts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      imageUrl:
+        "https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png",
+      username: "cleverqazi",
+      caption: "WOW it works",
+    },
+    {
+      imageUrl: "https://firebase.google.com/images/social.png",
+      username: "rahul",
+      caption: "Secong one",
+    },
+  ]);
+
   return (
     <div className="app">
-
       {/* Header */}
       <div className="app__header">
-        <img className="app__headerImage"
+        <img
+          className="app__headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
           alt="Instagram"
-         />
+        />
       </div>
 
       <h1>Instagram clone</h1>
 
       {/* Posts */}
+
+      {posts.map((post) => (
+        <Post
+          imageUrl={post.imageUrl}
+          username={post.username}
+          caption={post.caption}
+        />
+      ))}
+
       {/* Posts */}
     </div>
   );
--- src/Post.js
+++ src/Post.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import "./Post.css";
+import Avatar from "@material-ui/core/avatar";
 
-function Post() {
+function Post({ imageUrl, username, caption }) {
   return (
-    <div>
-      I'm a post
+    <div className="post">
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt={username}
+          src="/static/images/avatar/1.jpg"
+        />
+        <h3>{username}</h3>
+      </div>
+
+      {/* header ->  avatar + username */}
+
+      {/* image */}
+      <img className="post__image" src={imageUrl} alt="UserImage" />
+
+      {/* username +  caption */}
+      <h4 className="post__text">
+        <strong>{username}</strong> {caption}
+      </h4>
     </div>
-  )
+  );
 }
 
-export default Post
+export default Post;
